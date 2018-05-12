283e02460b1ee4f554a99c4c2693db04a2e5651e
Julien Combattelli <julien.combattelli@gmail.com>
2018-05-12 18:59:52 +0200

    Rename shell_io functions

--- shell/shell_io.h
+++ shell/shell_io.h
@@ -1,20 +1,20 @@
 #ifndef SHELL_IO_H_
 #define SHELL_IO_H_
 
-char shell_get_char(void);
+char shell_io_get_char(void);
 
-void shell_put_char(char c);
+void shell_io_put_char(char c);
 
-void shell_put_newline(void);
+void shell_io_put_newline(void);
 
-void shell_put_string(const char *str);
+void shell_io_put_string(const char *str);
 
-void shell_put_buffer(const char *str, int size);
+void shell_io_put_buffer(const char *str, int size);
 
-void shell_print_prompt(void);
+void shell_io_print_prompt(void);
 
-void shell_erase_last_char(void);
+void shell_io_erase_last_char(void);
 
-void shell_erase_line(void);
+void shell_io_erase_line(void);
 
 #endif /* SHELL_IO_H_ */
