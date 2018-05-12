73450f89b9e647ed97c2fdbb3ae7822909efdb80
U-DESKTOP-KEGGPFP\jucom <julien.combattelli@hotmail.com>
2018-05-12 18:59:52 +0200

    Rename shell_io functions

--- shell/shell_io.c
+++ shell/shell_io.c
@@ -1,39 +1,41 @@
 #include "shell_config.h"
 #include "shell_io.h"
+#include <stdio.h>
+#include <string.h>
 
-char shell_get_char(void)
+char shell_io_get_char(void)
 {
     return getchar();
 }
 
-void shell_put_char(char c)
+void shell_io_put_char(char c)
 {
     putchar(c);
 }
 
-void shell_put_newline(void)
+void shell_io_put_newline(void)
 {
-    shell_put_char('\r');
-    shell_put_char('\n');
+    shell_io_put_char('\r');
+    shell_io_put_char('\n');
 }
 
-void shell_put_string(const char *str)
+void shell_io_put_string(const char *str)
 {
-    shell_put_buffer(str, strlen(str));
+    shell_io_put_buffer(str, strlen(str));
 }
 
-void shell_put_buffer(const char *str, int size)
+void shell_io_put_buffer(const char *str, int size)
 {
     for (int i = 0; i < size; i++)
-        shell_put_char(str[i]);
+        shell_io_put_char(str[i]);
 }
 
-void shell_print_prompt(void)
+void shell_io_print_prompt(void)
 {
-    shell_put_string(SHELL_DEFAULT_PROMPT);
+    shell_io_put_string(SHELL_DEFAULT_PROMPT);
 }
 
-void shell_erase_last_char(void)
+void shell_io_erase_last_char(void)
 {
     // go back to one character
     putchar('\b');
@@ -43,7 +45,7 @@ void shell_erase_last_char(void)
     putchar('\b');
 }
 
-void shell_erase_line(void)
+void shell_io_erase_line(void)
 {
     // TODO: use escape sequence
 }
