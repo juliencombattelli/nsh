f8f3d94fb4f62b7a24ab6c4735d7143407d26559
Julien Combattelli <julien.combattelli@gmail.com>
2018-05-12 19:00:41 +0200

    Move some shell functions into shell_cmd module

--- shell/shell_cmd.c
+++ shell/shell_cmd.c
@@ -8,17 +8,51 @@
 #include "shell_cmd.h"
 #include <string.h>
 
-void shell_cmdcpy(shell_cmd_t *dst, const shell_cmd_t *src)
+void shell_cmd_copy(shell_cmd_t *dst, const shell_cmd_t *src)
 {
     dst->handler = src->handler;
     strcpy(dst->name, src->name);
 }
 
-void shell_cmdswap(shell_cmd_t *cmd1, shell_cmd_t *cmd2)
+void shell_cmd_swap(shell_cmd_t *cmd1, shell_cmd_t *cmd2)
 {
     shell_cmd_t temp;
-    shell_cmdcpy(&temp, cmd1);
-    shell_cmdcpy(cmd1, cmd2);
-    shell_cmdcpy(cmd2, &temp);
+    shell_cmd_copy(&temp, cmd1);
+    shell_cmd_copy(cmd1, cmd2);
+    shell_cmd_copy(cmd2, &temp);
 }
 
+void shell_cmd_lexicographic_sort(shell_cmd_array_t *cmds)
+{
+   for (int i = 0; i < cmds->count - 1; ++i)
+       for (int j = i + 1; j < cmds->count; ++j)
+           if (strcmp(cmds->array[i].name, cmds->array[j].name) > 0)
+               shell_cmd_swap(&cmds->array[i], &cmds->array[j]);
+}
+
+shell_cmd_t* shell_cmd_find_matching(shell_cmd_array_t *cmds, const char *partial_name, int name_size)
+{
+    for (int i = 0; i < cmds->count; i++)
+        if (memcmp(partial_name, cmds->array[i].name, name_size) == 0)
+            return &cmds->array[i];
+    return NULL;
+}
+
+shell_cmd_t* shell_cmd_find(shell_cmd_array_t *cmds, const char *name)
+{
+    return shell_cmd_find_matching(cmds, name, strlen(name));
+}
+
+int shell_cmd_register(shell_cmd_array_t *cmds, const char *name, shell_cmd_handler_t handler)
+{
+    size_t name_len = strlen(name);
+    if (name_len == 0 || name_len > SHELL_CMD_NAME_SIZE || !handler)
+        return SHELL_STATUS_WRONG_ARG;
+
+    strcpy(cmds->array[cmds->count].name, name);
+    cmds->array[cmds->count].handler = handler;
+
+    cmds->count++;
+
+    return SHELL_STATUS_OK;
+}
