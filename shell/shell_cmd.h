f6c55e0e32db1f2c116ced33d50d0ed8cf12dfe0
U-DESKTOP-KEGGPFP\jucom <julien.combattelli@hotmail.com>
2018-05-12 19:00:41 +0200

    Move some shell functions into shell_cmd module

--- shell/shell_cmd.h
+++ shell/shell_cmd.h
@@ -8,10 +8,11 @@
 #ifndef SHELL_CMD_H_
 #define SHELL_CMD_H_
 
+#include "shell_common_defs.h"
 #include "shell_config.h"
 
 typedef char shell_cmd_name_t[SHELL_CMD_NAME_SIZE];
-typedef int (*shell_cmd_handler_t)(int, char**);
+typedef int (*shell_cmd_handler_t)(int, char*[]);
 
 typedef struct shell_cmd
 {
@@ -20,7 +21,19 @@ typedef struct shell_cmd
 
 } shell_cmd_t;
 
-void shell_cmdcpy(shell_cmd_t *dst, const shell_cmd_t *src);
-void shell_cmdswap(shell_cmd_t *cmd1, shell_cmd_t *cmd2);
+typedef struct shell_cmd_array
+{
+	shell_cmd_t array[SHELL_CMD_MAX_COUNT];
+	int count;
+
+} shell_cmd_array_t;
+
+void shell_cmd_copy(shell_cmd_t *dst, const shell_cmd_t *src);
+void shell_cmd_swap(shell_cmd_t *cmd1, shell_cmd_t *cmd2);
+
+void shell_cmd_lexicographic_sort(shell_cmd_array_t *cmds);
+shell_cmd_t* shell_cmd_find_matching(shell_cmd_array_t *cmds, const char *partial_name, int name_size);
+shell_cmd_t* shell_cmd_find(shell_cmd_array_t *cmds, const char *name);
+int shell_cmd_register(shell_cmd_array_t *cmds, const char *name, shell_cmd_handler_t handler);
 
 #endif /* SHELL_CMD_H_ */
