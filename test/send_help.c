c7a41889bd6fb4fffbbeb6d520100dc3d91dd267
jucom <julien.combattelli@hotmail.com>
2018-07-13 13:25:21 +0200

    Add test programs for unix platform

---
+++ test/send_help.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    sleep(1);
+    puts("help");
+    sleep(1);
+    puts("exit");
+    return 0;
+}
+
---
+++ test/simple_shell.c
@@ -0,0 +1,12 @@
+#include <shell.h>
+#include <stdio.h>
+
+int main(void)
+{
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    shell_init();
+    run_shell();
+    return 0;
+}
+
