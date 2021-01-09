7848aea69b05cb3705cbf7546b069e289cebde3f
Julien Combattelli <julien.combattelli@gmail.com>
2021-01-09 18:51:24 +0100

    Fix missing braces from clang-tidy

--- src/nsh_line_buffer.c
+++ src/nsh_line_buffer.c
@@ -10,8 +10,9 @@ void nsh_line_buffer_reset(nsh_line_buffer_t* linebuf)
 
 int nsh_line_buffer_append_char(nsh_line_buffer_t* linebuf, char c)
 {
-    if (nsh_line_buffer_is_full(linebuf))
+    if (nsh_line_buffer_is_full(linebuf)) {
         return NSH_STATUS_BUFFER_OVERFLOW;
+    }
 
     linebuf->buffer[linebuf->size++] = c;
     return NSH_STATUS_OK;
@@ -19,16 +20,18 @@ int nsh_line_buffer_append_char(nsh_line_buffer_t* linebuf, char c)
 
 void nsh_line_buffer_append_null(nsh_line_buffer_t* linebuf)
 {
-    if (nsh_line_buffer_is_full(linebuf))
+    if (nsh_line_buffer_is_full(linebuf)) {
         linebuf->buffer[NSH_LINE_BUFFER_SIZE - 1] = '\0'; // overwrite last char to ensure the buffer is null terminated
-    else
+    } else {
         nsh_line_buffer_append_char(linebuf, '\0');
+    }
 }
 
 void nsh_line_buffer_erase_last_char(nsh_line_buffer_t* linebuf)
 {
-    if (!nsh_line_buffer_is_empty(linebuf))
+    if (!nsh_line_buffer_is_empty(linebuf)) {
         linebuf->size--;
+    }
 }
 
 bool nsh_line_buffer_is_full(nsh_line_buffer_t* linebuf)
