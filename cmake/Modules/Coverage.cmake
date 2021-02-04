7bc9167829a707f87621baca4974d7a15d1179bc
Julien Combattelli <julien.combattelli@gmail.com>
2021-02-04 22:10:55 +0100

    Add coverage target and doc

--- cmake/Modules/Coverage.cmake
+++ cmake/Modules/Coverage.cmake
@@ -9,9 +9,29 @@ endfunction()
 if(ENABLE_COVERAGE)
     _nsh_assert_valid_build_type_for_coverage()
 
-    # add --coverage option to Nsh::Nsh if gnu compiler
+    target_compile_options(nsh
+        PRIVATE
+            "$<$<C_COMPILER_ID:GNU>:--coverage>"
+    )
+    # target_link_options does not work to add coverage link flag because nsh is a static lib...
+    # target_link_options(nsh
+    #     PRIVATE
+    #         "$<$<C_COMPILER_ID:GNU>:--coverage>"
+    # )
+    # ...so use target_link_libraries instead
+    target_link_libraries(nsh
+        PRIVATE
+            "$<$<C_COMPILER_ID:GNU>:--coverage>"
+    )
 
-    # define custom target starting coverage like in https://www.labri.fr/perso/allali/?page_id=1146
+    # TODO add FindLCOV find module
+    # Checkout https://github.com/RWTH-HPC/CMake-codecov for some design ideas
+    add_custom_target(coverage
+        COMMAND lcov --capture --directory . --output-file coverage.info
+        COMMAND lcov --directory . --output-file coverage.info --remove coverage.info "/usr/" ".moc" "test/*"
+        COMMAND genhtml -o coverage coverage.info
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    )
 
 endif()
 
