e9c8a0b6dfd37ff24716116ec627fefb26f50832
Julien Combattelli <julien.combattelli@gmail.com>
2021-02-06 22:59:28 +0100

    Use latest GTest commit

--- cmake/Modules/GoogleTest.cmake
+++ cmake/Modules/GoogleTest.cmake
@@ -7,7 +7,7 @@ if(ENABLE_TESTS)
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY  https://github.com/google/googletest.git
-        GIT_TAG         release-1.10.0
+        GIT_TAG         master
         GIT_SHALLOW     TRUE
         PATCH_COMMAND   ${NSH_GTEST_PATCH_COMMAND}
     )
