7f8bfe476a4cc6d54ecc1a48fa58841c7fc71cfc
Julien Combattelli <julien.combattelli@gmail.com>
2021-02-07 13:36:03 +0100

    Add more utests for cmd_array module

--- test/units/test_nsh_cmd_array.cpp
+++ test/units/test_nsh_cmd_array.cpp
@@ -63,3 +63,94 @@ TEST(NshCmdArrayRegister, FailureTooManyElements)
 
     ASSERT_EQ(status, NSH_STATUS_MAX_CMD_NB_REACH);
 }
+
+TEST(NshCmdArrayFindMatching, Success)
+{
+    nsh_cmd_array_t cmds;
+    ASSERT_EQ(nsh_cmd_array_init(&cmds), NSH_STATUS_OK);
+
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd1_test", &cmd_test_handler), NSH_STATUS_OK);
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd2_test", &cmd_test_handler), NSH_STATUS_OK);
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd3_test", &cmd_test_handler), NSH_STATUS_OK);
+
+    static constexpr const char searched_for[] = "cmd2_";
+    auto* cmd = nsh_cmd_array_find_matching(&cmds, searched_for, sizeof(searched_for) - 1);
+
+    ASSERT_NE(cmd, nullptr);
+    ASSERT_STREQ(cmd->name, "cmd2_test");
+}
+
+TEST(NshCmdArrayFindMatching, FailureEmpty)
+{
+    nsh_cmd_array_t cmds;
+    ASSERT_EQ(nsh_cmd_array_init(&cmds), NSH_STATUS_OK);
+
+    static constexpr const char searched_for[] = "cmd2_";
+    auto* cmd = nsh_cmd_array_find_matching(&cmds, searched_for, sizeof(searched_for));
+
+    ASSERT_EQ(cmd, nullptr);
+}
+
+TEST(NshCmdArrayFindMatching, FailurePopulated)
+{
+    nsh_cmd_array_t cmds;
+    ASSERT_EQ(nsh_cmd_array_init(&cmds), NSH_STATUS_OK);
+
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd1_test", &cmd_test_handler), NSH_STATUS_OK);
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd2_test", &cmd_test_handler), NSH_STATUS_OK);
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd3_test", &cmd_test_handler), NSH_STATUS_OK);
+
+    static constexpr const char searched_for[] = "cmd2_";
+    auto* cmd = nsh_cmd_array_find_matching(&cmds, searched_for, sizeof(searched_for));
+
+    ASSERT_EQ(cmd, nullptr);
+}
+
+TEST(NshCmdArrayFind, Success)
+{
+    nsh_cmd_array_t cmds;
+    ASSERT_EQ(nsh_cmd_array_init(&cmds), NSH_STATUS_OK);
+
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd1_test", &cmd_test_handler), NSH_STATUS_OK);
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd2_test", &cmd_test_handler), NSH_STATUS_OK);
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd3_test", &cmd_test_handler), NSH_STATUS_OK);
+
+    static constexpr const char searched_for[] = "cmd2_test";
+    auto* cmd = nsh_cmd_array_find(&cmds, searched_for);
+
+    ASSERT_NE(cmd, nullptr);
+    ASSERT_STREQ(cmd->name, "cmd2_test");
+}
+
+static int cmd1(int, char**)
+{
+    return 1;
+}
+static int cmd2(int, char**)
+{
+    return 2;
+}
+static int cmd3(int, char**)
+{
+    return 3;
+}
+
+TEST(NshCmdArrayLexicographicSort, Success)
+{
+    nsh_cmd_array_t cmds;
+    ASSERT_EQ(nsh_cmd_array_init(&cmds), NSH_STATUS_OK);
+
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd3_test", &cmd3), NSH_STATUS_OK);
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd1_test", &cmd1), NSH_STATUS_OK);
+    ASSERT_EQ(nsh_cmd_array_register(&cmds, "cmd2_test", &cmd2), NSH_STATUS_OK);
+
+    auto status = nsh_cmd_array_lexicographic_sort(&cmds);
+    ASSERT_EQ(status, NSH_STATUS_OK);
+
+    ASSERT_STREQ(cmds.array[0].name, "cmd1_test");
+    ASSERT_EQ(cmds.array[0].handler, &cmd1);
+    ASSERT_STREQ(cmds.array[1].name, "cmd2_test");
+    ASSERT_EQ(cmds.array[1].handler, &cmd2);
+    ASSERT_STREQ(cmds.array[2].name, "cmd3_test");
+    ASSERT_EQ(cmds.array[2].handler, &cmd3);
+}
