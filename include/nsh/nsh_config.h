28c83e1850052fb9fe5ede9ad469d3a1d9d950d3
Julien Combattelli <julien.combattelli@gmail.com>
2021-03-14 21:32:38 +0100

    Let the config be modified by the build system

--- include/nsh/nsh_config.h
+++ include/nsh/nsh_config.h
@@ -17,21 +17,27 @@
  * If you exceed this number, the read line function will return the status
  * NSH_STATUS_BUFFER_OVERFLOW, and a warning will be displayed.
  */
+#ifndef NSH_LINE_BUFFER_SIZE
 #define NSH_LINE_BUFFER_SIZE 128u
+#endif
 
 /*
  * Maximum character count for commands name and arguments string.
  * If you try to register a command with a name greater than this,
  * the registration function will return NSH_STATUS_WRONG_ARG.
  */
+#ifndef NSH_MAX_STRING_SIZE
 #define NSH_MAX_STRING_SIZE 16u
+#endif
 
 /*
  * Maximum number of command you can register in nsh.
  * If you reach this number, all registration request will be ignored and
  * the registration function will return NSH_STATUS_MAX_CMD_NB_REACH.
  */
+#ifndef NSH_CMD_MAX_COUNT
 #define NSH_CMD_MAX_COUNT 32u
+#endif
 
 /*
  * Maximum number of arguments you can write in a command line.
@@ -40,7 +46,9 @@
  * If you reach this number, the argument line split function will return with
  * the status NSH_STATUS_MAX_ARGS_NB_REACH.
  */
+#ifndef NSH_CMD_ARGS_MAX_COUNT
 #define NSH_CMD_ARGS_MAX_COUNT 32u
+#endif
 
 /*
  * Maximum number of command memorized into the history.
@@ -50,34 +58,46 @@
  * in BSS region, which can be pretty huge...
  * Require: NSH_FEATURE_USE_HISTORY == 1
  */
+#ifndef NSH_CMD_HISTORY_SIZE
 #define NSH_CMD_HISTORY_SIZE 16u
+#endif
 
 /*
  * Default prompt displayed at the beginning of each command line.
  */
+#ifndef NSH_DEFAULT_PROMPT
 #define NSH_DEFAULT_PROMPT "> "
+#endif
 
 /*
  * Allow command auto-completion using tabulation key.
  */
+#ifndef NSH_FEATURE_USE_AUTOCOMPLETION
 #define NSH_FEATURE_USE_AUTOCOMPLETION 1
+#endif
 
 /*
  * Allow command memorization and navigation through the history using up and
  * down arrows.
  */
+#ifndef NSH_FEATURE_USE_HISTORY
 #define NSH_FEATURE_USE_HISTORY 1
+#endif
 
 /*
  * Define a printf-like function, which can be resource hungry...
  */
+#ifndef NSH_FEATURE_USE_PRINTF
 #define NSH_FEATURE_USE_PRINTF 1
+#endif
 
 /*
  * Allow command return code printing (for debug purpose)
  * Require: NSH_FEATURE_USE_PRINTF == 1
  */
+#ifndef NSH_FEATURE_USE_RETURN_CODE_PRINTING
 #define NSH_FEATURE_USE_RETURN_CODE_PRINTING 1
+#endif
 
 /******************************************************************************
  *** Internal configuration section (DO NOT TOUCH!)
