849ce5bc69533d20b28ea8c83c584e0e5754baaa
Julien Combattelli <julien.combattelli@gmail.com>
2021-06-12 10:48:15 +0200

    Remove useless test in gtest cmake script

--- cmake/Modules/GoogleTest.cmake
+++ cmake/Modules/GoogleTest.cmake
@@ -1,19 +1,17 @@
 # FetchContent_MakeAvailable was added in CMake 3.14
 cmake_minimum_required(VERSION 3.14)
 
-if(ENABLE_TESTS)
-    include(FetchContent)
+include(FetchContent)
 
-    FetchContent_Declare(
-        googletest
-        GIT_REPOSITORY  https://github.com/google/googletest.git
-        GIT_TAG         master
-        GIT_SHALLOW     TRUE
-        PATCH_COMMAND   ${NSH_GTEST_PATCH_COMMAND}
-    )
+FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY  https://github.com/google/googletest.git
+    GIT_TAG         master
+    GIT_SHALLOW     TRUE
+    PATCH_COMMAND   ${NSH_GTEST_PATCH_COMMAND}
+)
 
-    FetchContent_MakeAvailable(googletest)
+FetchContent_MakeAvailable(googletest)
 
-    target_link_libraries(gtest PUBLIC Nsh::Platform)
-    target_link_libraries(gtest PRIVATE Nsh::Platform::GTest)
-endif()
+target_link_libraries(gtest PUBLIC Nsh::Platform)
+target_link_libraries(gtest PRIVATE Nsh::Platform::GTest)
