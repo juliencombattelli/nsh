dcda0c22677b88244da108429ee8b0f107e6c78f
Julien Combattelli <julien.combattelli@gmail.com>
2022-02-27 19:46:00 +0100

    Update Nsh test files

--- test/nshpp/include/nsh.hpp
+++ test/nshpp/include/nsh.hpp
@@ -15,192 +15,202 @@
 
 namespace nsh {
 
-/**
- * @brief Check if all arguments in parameter pack U are same as T.
- */
-template<typename T, typename... U>
-inline constexpr bool AllSame = std::conjunction_v<std::is_same<T, U>...>;
+// /**
+//  * @brief Check if all arguments in parameter pack U are same as T.
+//  */
+// template<typename T, typename... U>
+// inline constexpr bool AllSame = std::conjunction_v<std::is_same<T, U>...>;
 
-/**
- * @brief Shell command class associating a name and a command to execute.
- * @tparam THandler Type of the callable to execute when the command is invoked.
- *
- * THandler can be any callable type like pointer to function, functor, std::function.
- */
-template<typename THandler>
-struct Cmd
+// /**
+//  * @brief Shell command class associating a name and a command to execute.
+//  * @tparam THandler Type of the callable to execute when the command is invoked.
+//  *
+//  * THandler can be any callable type like pointer to function, functor, std::function.
+//  */
+// template<typename THandler>
+// struct Cmd
+// {
+//     using HandlerType = THandler;
+//     std::string_view name;
+//     THandler handler;
+// };
+
+// /**
+//  * @brief List of commands that a nsh shell can use.
+//  * @tparam TCmd Type of the commands of the list.
+//  * @tparam N Commands count in the list.
+//  *
+//  * This class behaves like a sorted std::array of N TCmds.
+//  * It cannot be empty by design: it always contains at least one element.
+//  */
+// template<typename TCmd, std::size_t N>
+//     requires(N > 0)
+// class CmdList
+// {
+// public:
+//     using CmdType = TCmd;
+
+//     constexpr CmdList() = default;
+
+//     template<typename TCmdFirst, typename... TCmds>
+//         requires(AllSame<TCmdFirst, TCmds...>)
+//     constexpr explicit CmdList(TCmdFirst cmdfirst, TCmds... cmds) noexcept
+//         : list{ cmdfirst, cmds... }
+//     {
+//         std::ranges::sort(list, {}, &TCmdFirst::name);
+//     }
+
+//     constexpr auto begin() noexcept { return list.begin(); }
+//     constexpr auto begin() const noexcept { return list.begin(); }
+//     constexpr auto end() noexcept { return list.end(); }
+//     constexpr auto end() const noexcept { return list.end(); }
+//     constexpr auto size() const noexcept { return list.size(); }
+//     constexpr auto operator[](std::size_t pos) { return list[pos]; }
+//     constexpr auto operator[](std::size_t pos) const { return list[pos]; }
+
+// private:
+//     std::array<TCmd, N> list;
+// };
+
+// /**
+//  * @brief Deduction guide for CmdList template class.
+//  *
+//  * Allow the construction of the list without explicitly specifying its size.
+//  * The size template argument is deduced from the size of TCmds parameter pack.
+//  */
+// template<typename TCmdFirst, typename... TCmds>
+// explicit CmdList(TCmdFirst, TCmds...) -> CmdList<TCmdFirst, 1 + sizeof...(TCmds)>;
+
+// /**
+//  * @brief Concept specifying that a type can be used as a command autocompleter for nsh.
+//  * TODO remove dependency to TCmds
+//  */
+// template<typename T, typename TCmds>
+// concept Autocompleter = requires(T t, TCmds cmds, std::string_view str) {
+//                             // add default constructability
+//                             {
+//                                 t.autocomplete(cmds, str)
+//                             } /*-> std::same_as<std::span<typename TCmds::CmdType>>*/;
+//                         };
+
+// /**
+//  * @brief Concept specifying that a type can be used as a command suggester for nsh.
+//  */
+// template<typename T, typename TCmds>
+// concept Suggester = requires(T t, TCmds cmds, std::string_view str) {
+//                         // add default constructability
+//                         {
+//                             t.suggest(cmds, str)
+//                         } /*-> std::same_as<std::span<typename TCmds::CmdType>>*/;
+//                     };
+
+// struct NoAutocompleter
+// {};
+
+// struct BasicAutocompleter
+// {
+//     template<typename TCmdList>
+//     static constexpr auto autocomplete(TCmdList&& cmds, std::string_view str) noexcept
+//     {
+//         // Since list is already sorted lexicographically, all elements starting
+//         // with str are contiguous. So we search for the first one that matches,
+//         // the last one that does not match, and return a span from the first
+//         // element to the last one.
+//         const auto starts_with_str = [&](auto cmd) { return cmd.name.starts_with(str); };
+//         const auto first = std::find_if(cmds.begin(), cmds.end(), starts_with_str);
+//         const auto last = std::find_if_not(first, cmds.end(), starts_with_str);
+//         return std::span(first, last);
+//     }
+// };
+
+// template<std::size_t N>
+// constexpr std::size_t levenshteinDistance(
+//     std::string_view string_a, std::string_view string_b) noexcept
+// {
+//     const auto size_a = string_a.size();
+//     const auto size_b = string_b.size();
+
+//     if (size_a > N || size_b > N) {
+//         return std::numeric_limits<std::size_t>::max();
+//     }
+
+//     std::array<std::size_t, N> distances;
+//     std::iota(distances.begin(), distances.end(), std::size_t{ 0 });
+
+//     for (std::size_t i = 0; i < size_a; ++i) {
+//         std::size_t previous_distance = 0;
+//         for (std::size_t j = 0; j < size_b; ++j) {
+//             distances[j + 1] = std::min({
+//                 std::exchange(previous_distance, distances[j + 1])
+//                     + (string_a[i] == string_b[j] ? 0 : 1), // replacement cost
+//                 distances[j] + 1, // insertion cost
+//                 distances[j + 1] + 1 // deletion cost
+//             });
+//         }
+//     }
+//     return distances[size_b];
+// }
+
+// struct NoSuggester
+// {};
+
+// struct BasicSuggester
+// {
+//     template<typename TCmdList>
+//     static constexpr auto suggest(TCmdList&& cmds, std::string_view str) noexcept
+//     {
+//         std::size_t minimal_distance = std::numeric_limits<std::size_t>::max();
+//         std::size_t matching_command = 0;
+//         for (std::size_t i = 0; i < cmds.size(); ++i) {
+//             // TODO string size (16) should not be hardcoded here
+//             const auto distance = levenshteinDistance<16>(str, cmds[i].name);
+//             if (distance < minimal_distance) {
+//                 minimal_distance = distance;
+//                 matching_command = i;
+//             }
+//         }
+//         return cmds[matching_command].name;
+//     }
+// };
+
+// template<typename THandler>
+// struct BaseSettings
+// {
+//     using Handler = THandler;
+//     std::size_t LineBufferSize = 128;
+//     std::size_t MaxArgumentCount = 32;
+//     std::size_t CmdNameSize = 16;
+//     std::size_t CmdCount = 32;
+//     std::size_t HistorySize = 16;
+// };
+
+// template<auto settings, typename TAutocompleter = NoAutocompleter,
+//     typename TSuggester = NoSuggester>
+// struct DefaultAddOns
+// {
+//     [[no_unique_address]] TAutocompleter autocompleter;
+//     [[no_unique_address]] TSuggester suggester;
+// };
+
+// template<auto settings, typename TAddOns = DefaultAddOns<settings>>
+// class Nsh
+// {
+// public:
+//     using Settings = decltype(settings);
+//     using AddOns = TAddOns;
+
+//     AddOns addons;
+//     CmdList<Cmd<typename Settings::Handler>, settings.CmdCount> cmds;
+// };
+
+template<typename TIO, typename TBindings>
+struct Nsh
 {
-    using HandlerType = THandler;
-    std::string_view name;
-    THandler handler;
-};
-
-/**
- * @brief List of commands that a nsh shell can use.
- * @tparam TCmd Type of the commands of the list.
- * @tparam N Commands count in the list.
- *
- * This class behaves like a sorted std::array of N TCmds.
- * It cannot be empty by design: it always contains at least one element.
- */
-template<typename TCmd, std::size_t N>
-    requires(N > 0)
-class CmdList
-{
-public:
-    using CmdType = TCmd;
-
-    constexpr CmdList() = default;
-
-    template<typename TCmdFirst, typename... TCmds>
-        requires(AllSame<TCmdFirst, TCmds...>)
-    constexpr explicit CmdList(TCmdFirst cmdfirst, TCmds... cmds) noexcept
-        : list{ cmdfirst, cmds... }
+    void readline()
     {
-        std::ranges::sort(list, {}, &TCmdFirst::name);
+        auto c = TIO::get_char();
+        TBindings::at(c)();
     }
-
-    constexpr auto begin() noexcept { return list.begin(); }
-    constexpr auto begin() const noexcept { return list.begin(); }
-    constexpr auto end() noexcept { return list.end(); }
-    constexpr auto end() const noexcept { return list.end(); }
-    constexpr auto size() const noexcept { return list.size(); }
-    constexpr auto operator[](std::size_t pos) { return list[pos]; }
-    constexpr auto operator[](std::size_t pos) const { return list[pos]; }
-
-private:
-    std::array<TCmd, N> list;
-};
-
-/**
- * @brief Deduction guide for CmdList template class.
- *
- * Allow the construction of the list without explicitly specifying its size.
- * The size template argument is deduced from the size of TCmds parameter pack.
- */
-template<typename TCmdFirst, typename... TCmds>
-explicit CmdList(TCmdFirst, TCmds...) -> CmdList<TCmdFirst, 1 + sizeof...(TCmds)>;
-
-/**
- * @brief Concept specifying that a type can be used as a command autocompleter for nsh.
- * TODO remove dependency to TCmds
- */
-template<typename T, typename TCmds>
-concept Autocompleter = requires(T t, TCmds cmds, std::string_view str) {
-                            // add default constructability
-                            {
-                                t.autocomplete(cmds, str)
-                            } /*-> std::same_as<std::span<typename TCmds::CmdType>>*/;
-                        };
-
-/**
- * @brief Concept specifying that a type can be used as a command suggester for nsh.
- */
-template<typename T, typename TCmds>
-concept Suggester = requires(T t, TCmds cmds, std::string_view str) {
-                        // add default constructability
-                        {
-                            t.suggest(cmds, str)
-                        } /*-> std::same_as<std::span<typename TCmds::CmdType>>*/;
-                    };
-
-struct NoAutocompleter
-{};
-
-struct BasicAutocompleter
-{
-    template<typename TCmdList>
-    static constexpr auto autocomplete(TCmdList&& cmds, std::string_view str) noexcept
-    {
-        // Since list is already sorted lexicographically, all elements starting
-        // with str are contiguous. So we search for the first one that matches,
-        // the last one that does not match, and return a span from the first
-        // element to the last one.
-        const auto starts_with_str = [&](auto cmd) { return cmd.name.starts_with(str); };
-        const auto first = std::find_if(cmds.begin(), cmds.end(), starts_with_str);
-        const auto last = std::find_if_not(first, cmds.end(), starts_with_str);
-        return std::span(first, last);
-    }
-};
-
-template<std::size_t N>
-constexpr std::size_t levenshteinDistance(
-    std::string_view string_a, std::string_view string_b) noexcept
-{
-    const auto size_a = string_a.size();
-    const auto size_b = string_b.size();
-
-    if (size_a > N || size_b > N) {
-        return std::numeric_limits<std::size_t>::max();
-    }
-
-    std::array<std::size_t, N> distances;
-    std::iota(distances.begin(), distances.end(), std::size_t{ 0 });
-
-    for (std::size_t i = 0; i < size_a; ++i) {
-        std::size_t previous_distance = 0;
-        for (std::size_t j = 0; j < size_b; ++j) {
-            distances[j + 1] = std::min({
-                std::exchange(previous_distance, distances[j + 1])
-                    + (string_a[i] == string_b[j] ? 0 : 1), // replacement cost
-                distances[j] + 1, // insertion cost
-                distances[j + 1] + 1 // deletion cost
-            });
-        }
-    }
-    return distances[size_b];
-}
-
-struct NoSuggester
-{};
-
-struct BasicSuggester
-{
-    template<typename TCmdList>
-    static constexpr auto suggest(TCmdList&& cmds, std::string_view str) noexcept
-    {
-        std::size_t minimal_distance = std::numeric_limits<std::size_t>::max();
-        std::size_t matching_command = 0;
-        for (std::size_t i = 0; i < cmds.size(); ++i) {
-            // TODO string size (16) should not be hardcoded here
-            const auto distance = levenshteinDistance<16>(str, cmds[i].name);
-            if (distance < minimal_distance) {
-                minimal_distance = distance;
-                matching_command = i;
-            }
-        }
-        return cmds[matching_command].name;
-    }
-};
-
-template<typename THandler>
-struct BaseSettings
-{
-    using Handler = THandler;
-    std::size_t LineBufferSize = 128;
-    std::size_t MaxArgumentCount = 32;
-    std::size_t CmdNameSize = 16;
-    std::size_t CmdCount = 32;
-    std::size_t HistorySize = 16;
-};
-
-template<auto settings, typename TAutocompleter = NoAutocompleter,
-    typename TSuggester = NoSuggester>
-struct DefaultAddOns
-{
-    [[no_unique_address]] TAutocompleter autocompleter;
-    [[no_unique_address]] TSuggester suggester;
-};
-
-template<auto settings, typename TAddOns = DefaultAddOns<settings>>
-class Nsh
-{
-public:
-    using Settings = decltype(settings);
-    using AddOns = TAddOns;
-
-    AddOns addons;
-    CmdList<Cmd<typename Settings::Handler>, settings.CmdCount> cmds;
 };
 
 } // namespace nsh
--- test/nshpp/src/main.cpp
+++ test/nshpp/src/main.cpp
@@ -1,82 +1,118 @@
+#include "StaticBinding.hpp"
+#include "Stdio.hpp"
 #include "nsh.hpp"
 
-namespace my_shell {
+// namespace my_shell {
 
-struct my_context
+// struct my_context
+// {
+//     std::size_t some_data;
+// };
+
+// using my_handler = int (*)(my_context&, int, char**);
+// using my_cmd = nsh::Cmd<my_handler>;
+
+// constexpr nsh::BaseSettings<my_handler> my_settings {
+//     .LineBufferSize = 16,
+//     .MaxArgumentCount = 16,
+//     .CmdNameSize = 16,
+//     .CmdCount = 16,
+//     .HistorySize = 16,
+// };
+
+// constexpr my_cmd help("help", [](my_context&, int, char**) noexcept {
+//     puts("This is an helpful message!");
+//     return 42;
+// });
+
+// constexpr my_cmd exit("exit", [](my_context&, int, char**) noexcept {
+//     puts("Exiting!");
+//     return 69;
+// });
+
+// constexpr my_cmd toto("toto", [](my_context&, int, char**) noexcept {
+//     puts("Toto!");
+//     return 99;
+// });
+
+// constexpr my_cmd tata("tata", [](my_context&, int, char**) noexcept {
+//     puts("Tata!");
+//     return 99;
+// });
+
+// constexpr nsh::CmdList cmds(help, exit, toto, tata);
+
+// } // namespace my_shell
+
+// void autocomplete(auto shell, auto cmds, auto ctx, auto argc, auto argv, std::string_view str)
+// {
+//     using Autocompleter = decltype(shell.addons.autocompleter);
+//     constexpr bool has_autocomplete = !std::is_same_v<Autocompleter, nsh::NoAutocompleter>;
+//     if constexpr (has_autocomplete) {
+//         constexpr auto matching = shell.addons.autocompleter.autocomplete(cmds, str);
+//         for (const auto& cmd : matching) {
+//             cmd.handler(ctx, argc, argv);
+//         }
+//     }
+// }
+
+// void suggest(auto shell, auto cmds, std::string_view str)
+// {
+//     using Suggester = decltype(shell.addons.suggester);
+//     constexpr bool has_suggest = !std::is_same_v<Suggester, nsh::NoSuggester>;
+//     if constexpr (has_suggest) {
+//         constexpr auto suggestion = shell.addons.suggester.suggest(cmds, str);
+//         puts(suggestion.data());
+//     }
+// }
+
+void handle_0() noexcept
 {
-    std::size_t some_data;
-};
-
-using my_handler = int (*)(my_context&, int, char**);
-using my_cmd = nsh::Cmd<my_handler>;
-
-constexpr nsh::BaseSettings<my_handler> my_settings{
-    .LineBufferSize = 16,
-    .MaxArgumentCount = 16,
-    .CmdNameSize = 16,
-    .CmdCount = 16,
-    .HistorySize = 16,
-};
-
-constexpr my_cmd help("help", [](my_context&, int, char**) noexcept {
-    puts("This is an helpful message!");
-    return 42;
-});
-
-constexpr my_cmd exit("exit", [](my_context&, int, char**) noexcept {
-    puts("Exiting!");
-    return 69;
-});
-
-constexpr my_cmd toto("toto", [](my_context&, int, char**) noexcept {
-    puts("Toto!");
-    return 99;
-});
-
-constexpr my_cmd tata("tata", [](my_context&, int, char**) noexcept {
-    puts("Tata!");
-    return 99;
-});
-
-constexpr nsh::CmdList cmds(help, exit, toto, tata);
-
-} // namespace my_shell
-
-void autocomplete(auto shell, auto cmds, auto ctx, auto argc, auto argv, std::string_view str)
-{
-    using Autocompleter = decltype(shell.addons.autocompleter);
-    constexpr bool has_autocomplete = !std::is_same_v<Autocompleter, nsh::NoAutocompleter>;
-    if constexpr (has_autocomplete) {
-        constexpr auto matching = shell.addons.autocompleter.autocomplete(cmds, str);
-        for (const auto& cmd : matching) {
-            cmd.handler(ctx, argc, argv);
-        }
-    }
+    puts(__func__);
 }
 
-void suggest(auto shell, auto cmds, std::string_view str)
+void handle_1() noexcept
 {
-    using Suggester = decltype(shell.addons.suggester);
-    constexpr bool has_suggest = !std::is_same_v<Suggester, nsh::NoSuggester>;
-    if constexpr (has_suggest) {
-        constexpr auto suggestion = shell.addons.suggester.suggest(cmds, str);
-        puts(suggestion.data());
-    }
+    puts(__func__);
 }
+
+void handle_23()
+{
+    puts(__func__);
+}
+
+constexpr auto handle_4 = [] { puts(__func__); };
+
+void handle_eof() noexcept
+{
+    puts(__func__);
+}
+
+void handle_other() noexcept
+{
+    puts(__func__);
+}
+
+void invalid_handler(int)
+{
+}
+
+using MyNshBindings = nsh::StaticBinding<
+    nsh::BindByDefault<handle_other>,
+    nsh::Bind<0, handle_0>,
+    nsh::Bind<1, handle_1>,
+    nsh::Bind<2, handle_23>,
+    nsh::Bind<3, handle_23>,
+    nsh::Bind<4, handle_4>,
+    nsh::Bind<EOF, handle_eof>>;
 
 namespace nsh::tools {
 
-int main(int argc, char** argv)
+int main(int, char**)
 {
-    using namespace my_shell;
-
-    my_context ctx{ .some_data = 0xcafebabe };
-
-    nsh::Nsh<my_settings> nsh;
-
-    autocomplete(nsh, cmds, ctx, argc, argv, "t");
-
-    suggest(nsh, cmds, "toot");
+    Nsh<nsh::StdIo, MyNshBindings> nsh;
+    nsh.readline();
+    return 0;
 }
 
-}
+} // namespace nsh::tools
