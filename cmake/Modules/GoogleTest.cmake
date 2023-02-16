4f18acbc1a04fae09573f57f0a359f554d4b181a
Julien Combattelli <julien.combattelli@gmail.com>
2023-02-16 19:08:48 +0100

    Update GTest git branch

--- cmake/Modules/GoogleTest.cmake
+++ cmake/Modules/GoogleTest.cmake
@@ -6,7 +6,7 @@ include(FetchContent)
 FetchContent_Declare(
     googletest
     GIT_REPOSITORY  https://github.com/google/googletest.git
-    GIT_TAG         master
+    GIT_TAG         main
     GIT_SHALLOW     TRUE
     PATCH_COMMAND   ${NSH_GTEST_PATCH_COMMAND}
 )
