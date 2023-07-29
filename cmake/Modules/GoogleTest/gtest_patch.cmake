16fd93a09dbb43ca4af99a224c643de4836f52fa
Julien Combattelli <julien.combattelli@gmail.com>
2023-07-29 15:43:58 +0200

    Use git patch instead of CMake script to patch GTest

--- cmake/Modules/GoogleTest/gtest_patch.cmake
+++ cmake/Modules/GoogleTest/gtest_patch.cmake
@@ -2,7 +2,7 @@
 
 #[=======================================================================[.rst:
 gtest-patch
------------------
+-----------
 
 Patch googletest repository.
 Here is the list of the patches applied with the rationale behing them:
@@ -13,30 +13,12 @@ Here is the list of the patches applied with the rationale behing them:
 
 #]=======================================================================]
 
-set(IN_FILE googletest/src/gtest-internal-inl.h)
+find_package(Git REQUIRED)
 
-# create list of lines form the contens of a file
-file(STRINGS ${IN_FILE} LINES)
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} reset --hard
+)
 
-# overwrite the file....
-file(WRITE ${IN_FILE} "")
-
-# Known limitation with bracket handling in CMake lists
-# https://gitlab.kitware.com/cmake/cmake/-/issues/19156
-# Replace square brackets with arbitrary tokens to avoid the issue above
-string(REGEX REPLACE "\\[" "_NSH_BRACKET_OPEN" LINES "${LINES}")
-string(REGEX REPLACE "\\]" "_NSH_BRACKET_CLOSE" LINES "${LINES}")
-
-# loop through the lines, remove unwanted parts and write the (changed) line ...
-foreach(LINE IN LISTS LINES)
-    # Replace back the tokens with the brackets
-    string(REGEX REPLACE "_NSH_BRACKET_OPEN" "[" LINE "${LINE}")
-    string(REGEX REPLACE "_NSH_BRACKET_CLOSE" "]" LINE "${LINE}")
-
-    string(REPLACE
-        "#ifdef GTEST_HAS_FILE_SYSTEM"
-        "#if GTEST_HAS_FILE_SYSTEM"
-        LINE "${LINE}")
-
-    file(APPEND ${IN_FILE} "${LINE}\n")
-endforeach()
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} apply ${CMAKE_CURRENT_LIST_DIR}/0001-Use-if-to-check-filesystem-support.patch
+)
