2f64b5d502ee2f49cafc4f740c31e005bcec520a
Julien Combattelli <julien.combattelli@gmail.com>
2023-07-29 15:25:07 +0200

    Disable GTest shallow clone to fix checkout issue

--- cmake/Modules/GoogleTest.cmake
+++ cmake/Modules/GoogleTest.cmake
@@ -7,7 +7,7 @@ FetchContent_Declare(
     googletest
     GIT_REPOSITORY  https://github.com/google/googletest.git
     GIT_TAG         40412d85124f7c6f3d88454583c4633e5e10fc8c # TODO Update to 1.14 when released
-    GIT_SHALLOW     TRUE
+    GIT_SHALLOW     FALSE # Revert to TRUE when using a branch/tag instead of commit hash
     PATCH_COMMAND   ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_LIST_DIR}/GoogleTest/gtest_patch.cmake
 )
 
